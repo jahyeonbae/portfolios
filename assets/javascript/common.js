ab6b2ebccbdfc9ef078fae7e5c4a76dd1cc2c8b6
jahyeonbae <dingdongq911@gmail.com>
2017-08-09 22:55:02 +0900

    노트북

--- assets/javascript/common.js
+++ assets/javascript/common.js
@@ -1,21 +1,5 @@
 $(function(){
 
-<<<<<<< HEAD
-      $('.menu').mouseover(function(){
-         $('.slide').stop().slideDown();
-         console.log('ahajear')
-      })
-
-});
-      // $('.slide').mouseover(function(){
-      //    $('.slide').css('');
-      //    console.log('agaeh')
-
-
-    //  $('.navi').mouseover(function(){
-    //      $('.navi>span').animate('transform-origin','50%');
-    //  })
-=======
       $('.slide>ul>li').mouseover(function(){
          $(this).children('div').stop().slideDown()
        })
@@ -30,19 +14,15 @@ $(function(){
       $('.menu').mouseleave(function(){
         $(this).children('.bar').stop().animate({
           left:"-100px"}).css('display','none')
+       })
       })
-  })
 
       $('.navi').click(function(){
         $(this).next('div').stop().slideDown()
-       },function(){
-        $(this).next('div').stop().slideUp()
-
- })
-
+       })
+      $('.')
 
 
 
 
 });
->>>>>>> eeeacdfa614afcfe5f64904c66f42899681367d1
